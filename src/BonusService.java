be230b1dc6e4b4315bcbe884b258073a363803c2
Юлия Конышева <jukonysheva@gmail.com>
2022-07-28 19:28:06 +0300

    added new files

---
+++ src/BonusService.java
@@ -0,0 +1,7 @@
+public class BonusService {
+
+    public int calculate(int price) {
+        return price / 20;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        BonusService service = new BonusService();
+        int miles = service.calculate(14_000);
 
-        int startCheck = 100; //пополнил счет на 100 рублей
-        int startCheck1 = 1000; //пополнил счет на 1000 рублей
-        boolean task = true; //условие для получения бонуса
+        System.out.println("Бонусные мили: " + miles);
 
-        int percent;
-        if (task) {
-            percent = 1;
-        } else {
-            percent = 0;
-        }
 
-        int bonus = startCheck1 / 100 * percent;
-
-        if (startCheck1 < 1000) {
-
-            System.out.println("Бонус не получен");
-            System.out.println("Итого бонусов:" + (startCheck + startCheck1));
-
-        } else {
-            System.out.println("Бонусов получено:" + bonus);
-            System.out.println("Итого бонусов:" + (startCheck + startCheck1 + bonus));
-
-        }
     }
 }
